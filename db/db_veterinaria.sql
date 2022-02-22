af823835e9cdcbb3a58b0f0d27bce22402f153e8
miqueas-freiberger <miqueasfreiberger@gmail.com>
2022-02-22 13:32:53 -0300

    arreglos en la db

--- db/db_veterinaria.sql
+++ db/db_veterinaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-02-2022 a las 15:09:30
+-- Tiempo de generación: 22-02-2022 a las 17:32:21
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -36,6 +36,15 @@ CREATE TABLE `clientes` (
   `Localidad` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `NombreApellido`, `Telefono`, `Email`, `Direccion`, `Localidad`) VALUES
+(2, 'Miqueas Freiberger', 2147483647, 'miqueas@gmail.com', 'Burgos 221', 'Azul'),
+(3, 'Pepe Flores', 2147483647, 'pepe@mail.com', 'Leyria 2391', 'Azul'),
+(4, 'Omar Freiberger', 2147483647, 'omar1@mail.com', 'Burgos 881', 'Azul');
+
 -- --------------------------------------------------------
 
 --
@@ -62,13 +71,26 @@ CREATE TABLE `paciente` (
   `Sexo` varchar(30) NOT NULL,
   `Raza` varchar(50) NOT NULL,
   `Color` varchar(50) NOT NULL,
-  `Tamaño` varchar(10) NOT NULL,
+  `Tamano` varchar(10) NOT NULL,
   `Esterilizado` varchar(2) NOT NULL,
   `Complementarios` text NOT NULL,
   `Observaciones` text NOT NULL,
   `FechaIngreso` date NOT NULL,
+  `MotivoConsulta` text NOT NULL,
+  `Tratamiento` text NOT NULL,
   `id_dueño_fk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `paciente`
+--
+
+INSERT INTO `paciente` (`id`, `Nombre`, `Especie`, `Nacimiento`, `Sexo`, `Raza`, `Color`, `Tamano`, `Esterilizado`, `Complementarios`, `Observaciones`, `FechaIngreso`, `MotivoConsulta`, `Tratamiento`, `id_dueño_fk`) VALUES
+(1, 'Blacky', 'Canino', 2014, 'Hembra', 'Border Collie', 'Negro y Blanco', 'Mediano', 'Si', 'Analisis-sangre / Radiografia / Ecografia', 'blablablabalbalblablabllaasdjoaisjdiwomoidamdaosdmawodimawdoa;sdjaw;odjawd;oiajwd;aowdjaw;odjadow\r\nblablablabalbalblablabllaasdjoaisjdiwomoidamdaosdmawodimawdoa;sdjaw;odjawd;oiajwd;aowdjaw;odjadow', '2022-02-22', 'se rasca mucho', 'vacuna', 2),
+(2, 'Budin', 'Gatuno', 2015, 'Hembra', 'Gato', 'Marron', 'Chico', 'No', 'Analisis-sangre / Radiografia / Ecografia / Raspaje / Citologia', 'Ceborrea seca, Raspaje + Demodex y analisis de sangre(hemograma bien) (urea creatinina normal), hepatograma (fal =131,colest = 168,pret = 8,1)\r\nTSH = 0.64, T4Libre = 0,5', '2022-02-22', 'asdadsadsasd', 'dklasdhoiwuhi', 3),
+(3, 'Potente', 'Canino', 2018, 'Macho', 'Pitbull', 'Negro', 'Mediano', 'No', 'Analisis-sangre / Radiografia / Ecografia', 'blablablablablablalbalblablablalblbalablblalb', '2022-02-22', 'sjdwoiqndmoasnd', 'asdjhwhuioduihaosiudh', 4),
+(5, 'Pupi', 'Conejo', 2020, 'Hembra', 'Conejo', 'Blanco', 'chico', 'no', 'analisis-sangre / ecografia', 'el conejo tiene obesidad', '2022-02-22', 'Come mucho', 'dieta', 2),
+(6, 'Verdura', 'Tortuga', 2017, 'Macho', 'Tortuga', 'Marron', 'chico', 'no', 'radiografia', 'Tiene quebrado el caparazon', '2022-02-22', 'Lo pise con el auto', 'reposo y diclofenac en gel', 4);
 
 --
 -- Índices para tablas volcadas
@@ -102,7 +124,7 @@ ALTER TABLE `paciente`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `observaciones`
@@ -111,10 +133,11 @@ ALTER TABLE `observaciones`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `observaciones`
+-- AUTO_INCREMENT de la tabla `paciente`
 --
 ALTER TABLE `paciente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- Restricciones para tablas volcadas
 --
