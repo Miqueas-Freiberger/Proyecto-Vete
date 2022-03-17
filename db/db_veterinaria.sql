9e0274b35ccb3ef62447faf474c98b0e40db3a43
miqueas-freiberger <miqueasfreiberger@gmail.com>
2022-03-17 12:07:30 -0300

    cambios en la db

--- db/db_veterinaria.sql
+++ db/db_veterinaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-02-2022 a las 23:13:19
+-- Tiempo de generación: 17-03-2022 a las 16:06:43
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -45,7 +45,11 @@ INSERT INTO `clientes` (`id`, `NombreApellido`, `Telefono`, `Email`, `Direccion`
 (3, 'Pepe Flores', 2147483647, 'pepe@mail.com', 'Leyria 2391', 'Azul'),
 (4, 'Omar Freiberger', 2147483647, 'omar1@mail.com', 'Burgos 881', 'Azul'),
 (5, 'Juan Gonzalez', 2147483647, 'juan@mail.com', 'caneva 332', 'Azul'),
-(6, 'Juan Alberto Seisdedos', 2147483647, 'juana6@gmail.com', 'Calle Oscuridad 2210', 'Azul');
+(6, 'Juan Alberto Carlos 3', 2147483647, 'juanCarlo34@gmail.com', 'Calle Seca 230', 'Ayacucho'),
+(7, 'Federico Freiberger', 2147483647, 'frei.federico@gmail.com', 'Lavalle 1378', 'Azul'),
+(8, 'Brisa Caamaño', 2147483647, 'bricaam@gmail.com', 'belgrano 123', 'Azul'),
+(9, 'Juan Rodriguez', 228132012, 'juan@mail.com', 'calle falsa 123', 'Azul'),
+(11, 'Sebastian Freiberger', 2147483647, 'sebas@mail.com', 'calle 2 330', 'Azul');
 
 -- --------------------------------------------------------
 
@@ -58,6 +62,7 @@ CREATE TABLE `historial` (
   `Observacion` text NOT NULL,
   `MotivoConsulta` varchar(255) NOT NULL,
   `Tratamiento` text NOT NULL,
+  `Complementarios` varchar(80) NOT NULL,
   `Fecha` date NOT NULL,
   `id_mascota_fk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -66,9 +71,14 @@ CREATE TABLE `historial` (
 -- Volcado de datos para la tabla `historial`
 --
 
-INSERT INTO `historial` (`id`, `Observacion`, `MotivoConsulta`, `Tratamiento`, `Fecha`, `id_mascota_fk`) VALUES
-(8, 'Tiene la pata quebrada', 'Lo piso un auto', 'Entablilado y reposo', '2022-02-23', 1),
-(9, 'La pata no mejora', 'Sigue con dolor', 'Entablillado y reposo', '2022-03-02', 1);
+INSERT INTO `historial` (`id`, `Observacion`, `MotivoConsulta`, `Tratamiento`, `Complementarios`, `Fecha`, `id_mascota_fk`) VALUES
+(10, '-', 'Porque le dolia la panza', 'Inspeccion visual (?)', '', '2022-02-23', 14),
+(12, 'La gata es muy linda', 'Vomitos', 'Vacunas', '', '2022-03-06', 1072),
+(13, 'La gata mejoro', 'Revision', 'Ya esta curada', '', '2022-03-13', 1072),
+(14, 'La perra esta debil', 'Comio crema corporal', 'Inyecciones y antibiotico', '', '2022-03-06', 1073),
+(16, 'Esta en buen estado.', 'Se le cayo la cola', 'Ninguno', 'Analisis-sangre / Radiografia / Ecografia / Raspaje / Citologia', '2022-03-16', 1077),
+(17, '-', 'Chequeo', '-', 'Analisis-sangre / Radiografia', '2022-03-16', 1082),
+(18, 'Tiene un hueso mas largo que el otro', 'Segundo chequeo, radiografia', 'Cirugia', 'Radiografia', '2022-03-24', 1077);
 
 -- --------------------------------------------------------
 
@@ -86,7 +96,6 @@ CREATE TABLE `paciente` (
   `Color` varchar(50) NOT NULL,
   `Tamano` varchar(10) NOT NULL,
   `Esterilizado` varchar(2) NOT NULL,
-  `Complementarios` text NOT NULL,
   `FechaIngreso` date NOT NULL,
   `id_dueño_fk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -95,11 +104,14 @@ CREATE TABLE `paciente` (
 -- Volcado de datos para la tabla `paciente`
 --
 
-INSERT INTO `paciente` (`id`, `Nombre`, `Especie`, `Nacimiento`, `Sexo`, `Raza`, `Color`, `Tamano`, `Esterilizado`, `Complementarios`, `FechaIngreso`, `id_dueño_fk`) VALUES
-(1, 'Blacky', 'Canino', 2014, 'Hembra', 'Border Collie', 'Negro y Blanco', 'Mediano', 'Si', 'Analisis-sangre / Radiografia / Ecografia', '2022-02-22', 2),
-(2, 'Budin', 'Gatuno', 2015, 'Hembra', 'Gato', 'Marron', 'Chico', 'No', 'Analisis-sangre / Radiografia / Ecografia / Raspaje / Citologia', '2022-02-22', 3),
-(3, 'Potente', 'Canino', 2018, 'Macho', 'Pitbull', 'Negro', 'Mediano', 'No', 'Analisis-sangre / Radiografia / Ecografia', '2022-02-22', 4),
-(7, 'Snow', 'Canino', 2015, 'Macho', 'Husky', 'Gris', 'Grande', 'No', 'Analisis-sangre / Ecografia / Citologia', '2022-02-23', 5);
+INSERT INTO `paciente` (`id`, `Nombre`, `Especie`, `Nacimiento`, `Sexo`, `Raza`, `Color`, `Tamano`, `Esterilizado`, `FechaIngreso`, `id_dueño_fk`) VALUES
+(2, 'Budin', 'Gatuno', 2015, 'Hembra', 'Gato', 'Marron', 'Chico', 'No', '2022-02-22', 3),
+(3, 'Potente', 'Canino', 2018, 'Macho', 'Pitbull', 'Negro', 'Mediano', 'No', '2022-02-22', 4),
+(14, 'Poncho', 'Canino', 2017, 'Macho', 'Border Collie', 'Negro y blanco', 'Mediano', 'No', '2022-02-23', 7),
+(1072, 'Mora', 'Felino', 2012, 'Hembra', 'Gato', 'Negro y Blanco', 'Chico', 'Si', '2022-03-06', 8),
+(1073, 'India', 'Canino', 2018, 'Hembra', 'Beagle', 'Marron,Blanco,Negro', 'Mediano', 'No', '2022-03-06', 8),
+(1077, 'Juancho', 'Reptil', 2020, 'Macho', 'Lagarto', 'Verde ', 'Chico', 'Si', '2022-03-16', 11),
+(1082, 'Sancho', 'Oso', 2022, 'Macho', 'Panda', 'Negro y Blanco', 'Chico', 'No', '2022-03-16', 11);
 
 --
 -- Índices para tablas volcadas
@@ -133,19 +145,19 @@ ALTER TABLE `paciente`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `historial`
 --
 ALTER TABLE `historial`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `paciente`
 --
 ALTER TABLE `paciente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1083;
 
 --
 -- Restricciones para tablas volcadas
