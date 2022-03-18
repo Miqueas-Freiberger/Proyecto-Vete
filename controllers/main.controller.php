717662a2cf76502c9fc9b9cfcb8b33dd9769e160
miqueas-freiberger <miqueasfreiberger@gmail.com>
2022-03-18 12:06:53 -0300

    Identacion & orden en Controller, View, Model

--- controllers/main.controller.php
+++ controllers/main.controller.php
@@ -13,7 +13,7 @@ class MainController
         $this->mainModel = new MainModel();
         $this->mainView = new MainView();
     }
-
+    ///////////////////////////////////SHOW//////////////////////////////SHOW////////////////////////////////SHOW//////////////////////////////////////////////
     public function showHome()
     {
         $dataClientes = $this->mainModel->getDataClientes();
@@ -23,13 +23,7 @@ class MainController
     {
         $dataCliente = $this->mainModel->getDataCliente($id_cliente);
         $mascotasCliente = $this->mainModel->getDataMascotasCliente($id_cliente);
-        $this->mainView->displayClientInfo($dataCliente,$mascotasCliente,$id_cliente);
-    }
-
-    public function getHistorialMascota($id_mascota)
-    {
-        $historialMascota = $this->mainModel->getHistorialMascota($id_mascota);
-        $this->mainView->displayHistorialMascota($historialMascota,$id_mascota);
+        $this->mainView->displayClientInfo($dataCliente, $mascotasCliente, $id_cliente);
     }
 
     public function showClientsForms()
@@ -39,7 +33,13 @@ class MainController
 
     public function showMascotaForm($id_cliente)
     {
-        $this->mainView->displayMascotaForm($id_cliente);   
+        $this->mainView->displayMascotaForm($id_cliente);
+    }
+    ///////////////////////////////////GET//////////////////////////////GET////////////////////////////////GET//////////////////////////////////////////////////
+    public function getHistorialMascota($id_mascota)
+    {
+        $historialMascota = $this->mainModel->getHistorialMascota($id_mascota);
+        $this->mainView->displayHistorialMascota($historialMascota, $id_mascota);
     }
 
     public function getDataCliente()
@@ -65,26 +65,9 @@ class MainController
             $complementarios = implode(" / ", $_POST['complementarios']);
 
             $id_dueño = $this->addDataCliente($nombre_apellido, $telefono, $email, $direccion, $localidad);
-            $id_mascota= $this->addDataPaciente($nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril, $fecha_ingreso,$id_dueño);
-            $this->addHistorial($id_mascota,$observaciones,$motivoConsulta,$tratamiento, $complementarios,$fecha_ingreso,$id_dueño);
+            $id_mascota = $this->addDataPaciente($nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril, $fecha_ingreso, $id_dueño);
+            $this->addHistorial($id_mascota, $observaciones, $motivoConsulta, $tratamiento, $complementarios, $fecha_ingreso, $id_dueño);
         }
-    }
-
-    public function addDataCliente($nombre_apellido, $telefono, $email, $direccion, $localidad)
-    {
-        $id_cliente = $this->mainModel->addCliente($nombre_apellido, $telefono, $email, $direccion, $localidad);
-        return $id_cliente;
-    }
-    public function addDataPaciente($nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril, $fecha_ingreso,$id_dueño)
-    {
-        $id_mascota = $this->mainModel->addPaciente($nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril, $fecha_ingreso, $id_dueño);
-        return $id_mascota;        
-    }
-
-    public function eliminarMascota($id_mascota)
-    {
-        $this->mainModel->eliminarMascota($id_mascota);
-        header("Location: " . BASE_URL);
     }
 
     public function getDataMascota()
@@ -104,92 +87,114 @@ class MainController
             $fecha_ingreso = $_POST["fecha_ingreso"];
             $complementarios = implode(" / ", $_POST['complementarios']);
             $id_cliente = $_POST['id_cliente'];
-            
-            $id_mascota  = $this->addDataPaciente($nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril,$fecha_ingreso,$id_cliente);
-            $this->addHistorial($id_mascota,$observaciones,$motivoConsulta,$tratamiento,$complementarios,$fecha_ingreso,$id_cliente);
-        }        
+
+            $id_mascota  = $this->addDataPaciente($nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril, $fecha_ingreso, $id_cliente);
+            $this->addHistorial($id_mascota, $observaciones, $motivoConsulta, $tratamiento, $complementarios, $fecha_ingreso, $id_cliente);
+        }
     }
 
     public function getNombreCliente()
     {
-        $nombreCliente=$_GET["nombreCliente"];
+        $nombreCliente = $_GET["nombreCliente"];
         $cliente = $this->mainModel->buscarCliente($nombreCliente);
 
         if ($cliente) {
             $this->mainView->displayFilteredClient($cliente);
-        }else {
+        } else {
             $this->mainView->showError("No se encontraron clientes que coincidan con la busqueda", $nombreCliente);
         }
     }
 
-    public function displayFormsAddHistorial($id_mascota)
-    {
-        $this->mainView->displayHistorialForms($id_mascota);
-    }
-
     public function getNewHistorialData($id_historial = null)
     {
-        if ($id_historial==null)
-        {
+        if ($id_historial == null) {
             $observaciones = $_POST["observaciones"];
             $tratamiento = $_POST["tratamiento"];
             $motivoConsulta = $_POST["motivoConsulta"];
             $fecha = $_POST["fecha"];
             $id_mascota = $_POST["id_mascota"];
             $complementarios = implode(" / ", $_POST['complementarios']);
-            $this->addHistorial($id_mascota,$observaciones,$motivoConsulta,$tratamiento,$complementarios,$fecha);
-        }
-        else
-        {
+            $this->addHistorial($id_mascota, $observaciones, $motivoConsulta, $tratamiento, $complementarios, $fecha);
+        } else {
             $observaciones = $_POST["observaciones"];
             $tratamiento = $_POST["tratamiento"];
             $motivoConsulta = $_POST["motivoConsulta"];
             $fecha = $_POST["fecha"];
             $complementarios = implode(" / ", $_POST['complementarios']);
-            
-            $this->mainModel->updateHistorialData($observaciones,$tratamiento,$motivoConsulta,$fecha,$complementarios,$id_historial);
+
+            $this->mainModel->updateHistorialData($observaciones, $tratamiento, $motivoConsulta, $fecha, $complementarios, $id_historial);
+        }
+    }
+    ///////////////////////////////////ADD//////////////////////////////ADD////////////////////////////////ADD//////////////////////////////////////////////////
+
+    public function addDataCliente($nombre_apellido, $telefono, $email, $direccion, $localidad)
+    {
+        $id_cliente = $this->mainModel->addCliente($nombre_apellido, $telefono, $email, $direccion, $localidad);
+        return $id_cliente;
+    }
+    public function addDataPaciente($nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril, $fecha_ingreso, $id_dueño)
+    {
+        $id_mascota = $this->mainModel->addPaciente($nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril, $fecha_ingreso, $id_dueño);
+        return $id_mascota;
+    }
+
+    public function addHistorial($id_mascota, $observaciones, $motivoConsulta, $tratamiento, $complementarios, $fecha, $id_dueño = null)
+    {
+        $this->mainModel->addHistorial($id_mascota, $observaciones, $motivoConsulta, $tratamiento, $complementarios, $fecha);
+        if ($id_dueño) {
+            header("Location: " . "cliente" . "/$id_dueño");
+        } else {
+            header("Location: " . "historialMascota" . "/$id_mascota");
         }
     }
 
+    ///////////////////////////////////DELETE//////////////////////////////DELETE////////////////////////////////DELETE/////////////////////////////////////////////
 
-    public function addHistorial($id_mascota,$observaciones,$motivoConsulta,$tratamiento,$complementarios,$fecha,$id_dueño=null)
+    public function eliminarMascota($id_mascota)
     {
-        $this->mainModel->addHistorial($id_mascota,$observaciones,$motivoConsulta,$tratamiento,$complementarios,$fecha);
-        if ($id_dueño)
-        {
-            header("Location: " . "cliente" . "/$id_dueño");
-        }
-        else
-        {
-            header("Location: " . "historialMascota" . "/$id_mascota");
-        }
-        
+        $this->mainModel->eliminarMascota($id_mascota);
+        header("Location: " . BASE_URL);
+    }
+
+    ///////////////////////////////////DISPLAY//////////////////////////////DISPLAY////////////////////////////////DISPLAY/////////////////////////////////////////////
+
+    public function displayFormsAddHistorial($id_mascota)
+    {
+        $this->mainView->displayHistorialForms($id_mascota);
     }
 
     public function displayEditClienteForm($id_cliente)
     {
-        $dataCliente= $this->mainModel->getDataCliente($id_cliente);
+        $dataCliente = $this->mainModel->getDataCliente($id_cliente);
         $this->mainView->displayEditClientForm($dataCliente);
-    }
-
-    public function updateClientData()
-    {
-            $nombre_apellido = $_POST["nombre_apellido"];
-            $telefono = $_POST["telefono"];
-            $email = $_POST["email"];
-            $direccion = $_POST["direccion"];
-            $localidad = $_POST["localidad"];
-            $id_cliente = $_POST['id_cliente'];
-
-                $this->mainModel->updateClientData($nombre_apellido,$telefono,$email,$direccion,$localidad,$id_cliente);
-
-            header("Location: " . BASE_URL ."cliente" . "/$id_cliente");
     }
 
     public function displayEditMascotaForm($id_mascota)
     {
         $dataMascota = $this->mainModel->getDataMascota($id_mascota);
         $this->mainView->displayEditMascotaForm($dataMascota);
+    }
+
+    public function displayEditHistorialForm($id_historial)
+    {
+
+        $dataHistorial = $this->mainModel->getHistorialData($id_historial);
+        $this->mainView->displayEditHistorialForm($dataHistorial);
+    }
+
+    ///////////////////////////////////UPDATE//////////////////////////////UPDATE////////////////////////////////UPDATE/////////////////////////////////////////////
+
+    public function updateClientData()
+    {
+        $nombre_apellido = $_POST["nombre_apellido"];
+        $telefono = $_POST["telefono"];
+        $email = $_POST["email"];
+        $direccion = $_POST["direccion"];
+        $localidad = $_POST["localidad"];
+        $id_cliente = $_POST['id_cliente'];
+
+        $this->mainModel->updateClientData($nombre_apellido, $telefono, $email, $direccion, $localidad, $id_cliente);
+        header("Location: " . BASE_URL . "cliente" . "/$id_cliente");
     }
 
     public function updateDataMascota()
@@ -206,22 +211,11 @@ class MainController
         $id_mascota = $_POST["id_mascota"];
         $dataDueño = $this->mainModel->getIdDueño($id_mascota);
 
-        foreach ($dataDueño as $data)
-        {
+        foreach ($dataDueño as $data) {
             $id_dueño = $data->id_dueño_fk;
         }
-
-        
-            $this->mainModel->updateMascotaData($nombrePaciente,$especie,$nacimientoPaciente,$sexoPaciente,$raza,$color,$tamaño,$esteril,$fecha_ingreso,$id_mascota);
-
-        header("Location: " . BASE_URL ."cliente" . "/$id_dueño");
-    }
-
-
-    public function displayEditHistorialForm($id_historial)
-    {
-        $dataHistorial = $this->mainModel->getHistorialData($id_historial); 
-        $this->mainView->displayEditHistorialForm($dataHistorial);
+        $this->mainModel->updateMascotaData($nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril, $fecha_ingreso, $id_mascota);
+        header("Location: " . BASE_URL . "cliente" . "/$id_dueño");
     }
 
     public function updateDataHistorial()
@@ -230,7 +224,6 @@ class MainController
         $this->getNewHistorialData($id_historial);
 
         $id_mascota = $_POST["id_mascota_historial"];
-        header("Location: " . BASE_URL ."historialMascota" . "/$id_mascota");
+        header("Location: " . BASE_URL . "historialMascota" . "/$id_mascota");
     }
-
 }
--- models/main.model.php
+++ models/main.model.php
@@ -9,6 +9,8 @@ class MainModel
         $this->db = new PDO('mysql:host=localhost;' . 'dbname=db_veterinaria;charset=utf8', 'root', '');
     }
 
+    ///////////////////////////////////GET//////////////////////////////GET////////////////////////////////GET//////////////////////////////////////////////////
+
     public function getDataClientes()
     {
         $query = $this->db->prepare('SELECT * FROM clientes');
@@ -48,60 +50,16 @@ class MainModel
     }
     public function getInfoConsulta($id_mascota)
     {
-        $query=$this->db->prepare("SELECT Complementarios FROM paciente WHERE id = ?");
+        $query = $this->db->prepare("SELECT Complementarios FROM paciente WHERE id = ?");
         $query->execute([$id_mascota]);
 
         $queryData = $query->fetchAll(PDO::FETCH_OBJ);
         return $queryData;
-    }
-
-
-    public function addCliente($nombre_apellido, $telefono, $email,$direccion,$localidad)
-    {
-        $query = $this->db->prepare('INSERT INTO clientes (NombreApellido, Telefono, Email, Direccion,Localidad) VALUES (?,?,?,?,?)');
-        $query->execute([$nombre_apellido, $telefono, $email,$direccion,$localidad]);
-       
-        return $this->db->lastInsertId();
-    }
-
-    public function addPaciente($nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril,$fecha_ingreso,$id_dueño)
-    {
-        $query = $this->db->prepare("INSERT INTO paciente (Nombre,Especie,Nacimiento,Sexo,Raza,Color,Tamano,Esterilizado,FechaIngreso,id_dueño_fk) VALUES(?,?,?,?,?,?,?,?,?,?)");
-        $query->execute([$nombrePaciente,$especie,$nacimientoPaciente,$sexoPaciente,$raza,$color,$tamaño,$esteril,$fecha_ingreso,$id_dueño]);
-        
-        return $this->db->lastInsertId();
-    }
-
-    public function addHistorial($id_mascota,$observaciones,$motivoConsulta,$tratamiento,$complementarios,$fecha)
-    {
-        $query = $this->db->prepare("INSERT INTO historial (Observacion,MotivoConsulta,Tratamiento,Complementarios,Fecha,id_mascota_fk) VALUES (?,?,?,?,?,?)");
-        $query->execute([$observaciones,$motivoConsulta,$tratamiento,$complementarios,$fecha,$id_mascota]);
-    }
-
-    public function buscarCliente($nombreCliente)
-    {
-        $query=$this->db->prepare("SELECT * FROM clientes WHERE NombreApellido LIKE ?");
-        $query->execute(["%$nombreCliente%"]);
-
-        $queryData = $query->fetchAll(PDO::FETCH_OBJ);
-        return $queryData;
-    }
-
-    public function eliminarMascota($id_mascota)
-    {
-        $query=$this->db->prepare("DELETE FROM paciente WHERE id = ?");
-        $query->execute([$id_mascota]);
-    }
-
-    public function updateClientData($nombre_apellido,$telefono,$email,$direccion,$localidad,$id_cliente)
-    {
-        $query=$this->db->prepare("UPDATE clientes SET `NombreApellido` = ?, `Telefono` = ?, `Email`=?,`Direccion`=?,`Localidad`=? WHERE `id` = ?");
-        $query->execute([$nombre_apellido,$telefono,$email,$direccion,$localidad,$id_cliente]);
     }
 
     public function getDataMascota($id_mascota)
     {
-        $query=$this->db->prepare("SELECT * FROM paciente WHERE id = ?");
+        $query = $this->db->prepare("SELECT * FROM paciente WHERE id = ?");
         $query->execute([$id_mascota]);
 
         $queryData = $query->fetchAll(PDO::FETCH_OBJ);
@@ -119,12 +77,6 @@ class MainModel
         return $queryData;
     }
 
-    public function updateMascotaData($nombrePaciente,$especie,$nacimientoPaciente,$sexoPaciente,$raza,$color,$tamaño,$esteril,$fecha_ingreso,$id_mascota)
-    {
-        $query = $this->db->prepare("UPDATE paciente SET `Nombre`= ?, `Especie`= ?,`Nacimiento`=?, `Sexo`=?,`Raza`=?, `Color`=?,`Tamano`=?,`Esterilizado`=?,`FechaIngreso`=? WHERE `id` = ?");
-        $query->execute([$nombrePaciente,$especie,$nacimientoPaciente,$sexoPaciente,$raza,$color,$tamaño,$esteril,$fecha_ingreso,$id_mascota]); 
-    }
-
     public function getHistorialData($id_historial)
     {
         $query = $this->db->prepare('SELECT * FROM historial WHERE id = ?');
@@ -134,11 +86,67 @@ class MainModel
         return $queryData;
     }
 
-    public function updateHistorialData($observaciones,$tratamiento,$motivoConsulta,$fecha,$complementarios,$id_historial)
+    public function buscarCliente($nombreCliente)
     {
-       $query = $this->db->prepare("UPDATE historial SET `Observacion`=?,`MotivoConsulta`=?,`Tratamiento`=?,`Complementarios`=?,`Fecha`=? WHERE id = ?");
-       $query->execute([$observaciones,$motivoConsulta,$tratamiento,$complementarios,$fecha,$id_historial]);
+        $query = $this->db->prepare("SELECT * FROM clientes WHERE NombreApellido LIKE ?");
+        $query->execute(["%$nombreCliente%"]);
+
+        $queryData = $query->fetchAll(PDO::FETCH_OBJ);
+        return $queryData;
+    }
+
+    ///////////////////////////////////ADD//////////////////////////////ADD////////////////////////////////ADD//////////////////////////////////////////////////
+
+    public function addCliente($nombre_apellido, $telefono, $email, $direccion, $localidad)
+    {
+        $query = $this->db->prepare('INSERT INTO clientes (NombreApellido, Telefono, Email, Direccion,Localidad) VALUES (?,?,?,?,?)');
+        $query->execute([$nombre_apellido, $telefono, $email, $direccion, $localidad]);
+
+        return $this->db->lastInsertId();
+    }
+
+    public function addPaciente($nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril, $fecha_ingreso, $id_dueño)
+    {
+        $query = $this->db->prepare("INSERT INTO paciente (Nombre,Especie,Nacimiento,Sexo,Raza,Color,Tamano,Esterilizado,FechaIngreso,id_dueño_fk) VALUES(?,?,?,?,?,?,?,?,?,?)");
+        $query->execute([$nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril, $fecha_ingreso, $id_dueño]);
+
+        return $this->db->lastInsertId();
+    }
+
+    public function addHistorial($id_mascota, $observaciones, $motivoConsulta, $tratamiento, $complementarios, $fecha)
+    {
+        $query = $this->db->prepare("INSERT INTO historial (Observacion,MotivoConsulta,Tratamiento,Complementarios,Fecha,id_mascota_fk) VALUES (?,?,?,?,?,?)");
+        $query->execute([$observaciones, $motivoConsulta, $tratamiento, $complementarios, $fecha, $id_mascota]);
+    }
+
+    ///////////////////////////////////DELETE//////////////////////////////DELETE////////////////////////////////DELETE//////////////////////////////////////////////////
+
+    public function eliminarMascota($id_mascota)
+    {
+        $query = $this->db->prepare("DELETE FROM paciente WHERE id = ?");
+        $query->execute([$id_mascota]);
+    }
+
+    ///////////////////////////////////UPDATE//////////////////////////////UPDATE////////////////////////////////UPDATE//////////////////////////////////////////////////
+
+    public function updateClientData($nombre_apellido, $telefono, $email, $direccion, $localidad, $id_cliente)
+    {
+        $query = $this->db->prepare("UPDATE clientes SET `NombreApellido` = ?, `Telefono` = ?, `Email`=?,`Direccion`=?,`Localidad`=? WHERE `id` = ?");
+        $query->execute([$nombre_apellido, $telefono, $email, $direccion, $localidad, $id_cliente]);
     }
 
 
+
+    public function updateMascotaData($nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril, $fecha_ingreso, $id_mascota)
+    {
+        $query = $this->db->prepare("UPDATE paciente SET `Nombre`= ?, `Especie`= ?,`Nacimiento`=?, `Sexo`=?,`Raza`=?, `Color`=?,`Tamano`=?,`Esterilizado`=?,`FechaIngreso`=? WHERE `id` = ?");
+        $query->execute([$nombrePaciente, $especie, $nacimientoPaciente, $sexoPaciente, $raza, $color, $tamaño, $esteril, $fecha_ingreso, $id_mascota]);
+    }
+
+
+    public function updateHistorialData($observaciones, $tratamiento, $motivoConsulta, $fecha, $complementarios, $id_historial)
+    {
+        $query = $this->db->prepare("UPDATE historial SET `Observacion`=?,`MotivoConsulta`=?,`Tratamiento`=?,`Complementarios`=?,`Fecha`=? WHERE id = ?");
+        $query->execute([$observaciones, $motivoConsulta, $tratamiento, $complementarios, $fecha, $id_historial]);
+    }
 }
